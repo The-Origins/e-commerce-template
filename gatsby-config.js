8d353a7582cca4a11c1a78daf3548a9d91bdde37
Joshua <jwegahub@gmail.com>
2024-09-18 11:09:09 +0300

    updated x-frame-options

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,23 @@
  */
 module.exports = {
   siteMetadata: {
-    title:"E-commerce web template",
+    title: "E-commerce web template",
     description: "A web template for an e-commerce website",
     author: "Wegah Studios",
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/*": [
+            // Disable the X-Frame-Options by not setting any value
+            'X-Frame-Options: ""',
+            // Optionally, allow framing from anywhere using Content-Security-Policy
+            "Content-Security-Policy: frame-ancestors *",
+          ],
+        },
+      },
+    },
+  ],
+};
