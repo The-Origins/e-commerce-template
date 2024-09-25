59362cd178064e9c33aac539b8001d1042c660e8
Joshua <jwegahub@gmail.com>
2024-09-25 12:50:00 +0300

    updated gh-pages rules

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/e-commerce-template",  // Replace with your repo name
   siteMetadata: {
     title: "E-commerce web template",
     description: "A web template for an e-commerce website",
