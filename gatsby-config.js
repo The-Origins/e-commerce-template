9c977eeb4100aa9d0f921309fb55a43c0206baa1
Joshua <jwegahub@gmail.com>
2024-11-17 11:11:29 +0300

    updated comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  pathPrefix: "/e-commerce-template",  // Replace with your repo name
+  pathPrefix: "/e-commerce-template",  
   siteMetadata: {
     title: "E-commerce web template",
     description: "A web template for an e-commerce website",
