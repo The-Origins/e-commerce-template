17709f790b2ad2c349509c8d6ba17e5e4f4ea7be
Joshua <jwegahub@gmail.com>
2024-11-17 09:54:52 +0300

    build version

--- src/pages/category.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ResultsComponent from "../components/results";
-
-const CategoryPage = ({location, setConfirmationModal }) => {
-  return <ResultsComponent path={"category"} {...{location, setConfirmationModal }} />;
-};
-
-export default CategoryPage;
--- src/pages/results.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ResultsComponent from "../components/results";
-
-const ResultsPage = ({ location, setConfirmationModal }) => {
-  return <ResultsComponent {...{location, setConfirmationModal }} />;
-};
-
-export default ResultsPage;
