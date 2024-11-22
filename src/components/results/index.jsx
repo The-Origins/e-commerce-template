1b7fadd2ec163c8fb9a3db1be399f26a3dc0e235
Joshua <jwegahub@gmail.com>
2024-11-22 10:06:49 +0300

    minor bug fix

--- src/components/results/index.jsx
+++ src/components/results/index.jsx
@@ -126,7 +126,7 @@ const ResultsComponent = ({ location, path, setConfirmationModal }) => {
       setFilterOptions(resultsWorker.filterOptions);
       resetPage();
     }
-  }, [filters, data, params, offers]);
+  }, [filters, data,]);
 
   const resetPage = (page = 1) => {
     setParams((prev) => ({ ...prev, p: page }));
